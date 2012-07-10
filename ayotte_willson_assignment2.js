054f6123ba5d493647b846174050329d00383bdd
Ezcaflowne <ayotte.willson@gmail.com>
2012-07-09 23:31:23 -0700

    added boolean function.

--- ayotte_willson_assignment2.js
+++ ayotte_willson_assignment2.js
@@ -1,1 +1,27 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+// alert("JavaScript works!");
+
+// Name: Willson Ayotte
+// Date: July 28th, 2012
+// Document: Deliverable 2
+// Description: A day at the office
+
+console.log("A resident calls and said that he thinks his waterheater is leaking.")
+
+// Boolean Function
+
+// The waterheater did leak on the floor - output - call for water extraction
+// The waterheater did not leak on the floor - output - No action.
+var wetLeaking = "water heater has water coming from it.", 
+wetFloor = "The carpet is wet", 
+carpetExtraction = "Call for carpet extraction", 
+noAction = "No water can be found.";
+	if ((wetLeaking && wetFloor) || !(noAction)) {
+		console.log(carpetExtraction);
+	}	else {
+		console.log(noAction);
+
+}console.log("The carpet cleaners arrive at the residents apartment, and begin extraction the water from the carpet.");
+
+
