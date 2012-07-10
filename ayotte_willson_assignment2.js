9dc0e03d519906b5d83c2dcd5f3b9bdcec75abaf
Ezcaflowne <ayotte.willson@gmail.com>
2012-07-10 01:56:06 -0700

    Finished Array Function and cleaned up code.

--- ayotte_willson_assignment2.js
+++ ayotte_willson_assignment2.js
@@ -3,14 +3,13 @@
 // alert("JavaScript works!");
 
 // Name: Willson Ayotte
-// Date: July 28th, 2012
+// Date: July 10th, 2012
 // Document: Deliverable 2
 // Description: A day at the office
 
 console.log("A resident calls and said that he thinks his waterheater is leaking.")
 
 // Boolean Function
-
 var wetLeaking = "water heater has water coming from it.", 
 wetFloor = "The carpet is wet", 
 carpetExtraction = "Call for carpet extraction", 
@@ -20,11 +19,12 @@ noAction = "No water can be found.";
 	}	else {
 		console.log(noAction);
 
-}console.log("The carpet cleaners arrive at the residents apartment, and begin extraction the water from the carpet.");
+}
+
+console.log("The carpet cleaners arrive at the residents apartment, and begin extraction the water from the carpet.");
 
 // Number Function
 // While Loop
-
 var water = 0,
 	minutes = 0
 ;
@@ -37,31 +37,22 @@ while (water < 40) {
 console.log("No more water to extract.");
 
 
-
 // String Function
-
 var jobDone = "The job is done "
 var lookingForPayment = " the carpet cleaners are looking for payment."
 console.log(jobDone + "and" + lookingForPayment)
 
 // Array Function
-
-var payment1 = "Create a purchase order.",
-	payment2 = "Use petty cash.",
-	payment3 = "Charge the resident."
-;
-
+// For Loop
 var payments = [
-	"Create a purchase order.", 
-	"Use petty cash.", 
-	"Charge the resident."
+	"Log on to the computer,", 
+	"Create a purchase order,", 
+	"Give carpet cleaners the PO number",
+	"The transaction is complete."
 ];
 
-// payments[0]
+for (var i=0, p=payments.length; i < p; i++) {
+	console.log( payments[i] )
+}
 
-console.log ( payments[0] ); 
-
-// for (var water = 0; water < 41; water+=4){
-// 	if ( water >= 0 ) {minutes+=5};
-// 	console.log("The carpet cleaners extracted " + water + " gallons of water, for " + minutes + " minutes.");
-// };
+console.log("Now its time to kick back for a job well done.")
