58ce83ce580e7064d843af8cbc8a9c740e4b6ffe
Ezcaflowne <ayotte.willson@gmail.com>
2012-07-12 18:01:19 -0700

    Added Array function with for loop and moved calls to bottom of code.

--- ayotte_willson_assignment2.js
+++ ayotte_willson_assignment2.js
@@ -9,48 +9,29 @@
 var newWaterHeater = true;
 var noAction = true
 var total = 40;
-
-console.log('\"A resident calls and puts in a work order.\"')
-
-
+var workArray = ["badge", "wallet", "cell-phone", "tools"];
+var numberArray = 1
 
 // Procedure
-
-var getWorkRequest = function (workorder) 
-{
-	if (workorder === true)
-	{	
-		console.log ("Damn another work order, don't these things ever stop coming in...");
-	}
-	else 
-	{	
-		console.log ("I\'m not going to do this right now.");
-	}
-}
-
-
-getWorkRequest(true)
-
-
-
-
-
+var getWorkRequest = function(workorder) {
+	if (workorder === true) {	
+		console.log ("Damn another call, it's 3A.M. Don't these things ever stop coming in...");
+	} else {	
+		console.log ("I\'ll just call him back and say we will be over to look at it in the morning.");
+	};
+};// End Procedure
 
 // String Varible
 var waterHeater = function(whatYouNeed) {
     if (newWaterHeater === true) {
-        theNeed = "Looks like you need a " + whatYouNeed;
+        theNeed = "I see some water leaking on the floor. It looks like you need a " + whatYouNeed;
     } else {
-        theNeed = "Looks like it's a fuse.";
+        theNeed = "Hmmm... I dont see any water, I think it might be a something else.";
     };
     return theNeed;
-}; // end String Varible
-
-console.log (waterHeater("new water heater."));
-
+}; // End String Varible
 
 // Boolean Function
-
 var wet = function (noAction) {
 	var outcome;
 	var wetFloor = false
@@ -60,14 +41,7 @@ var wet = function (noAction) {
 		outcome = false
 	};
 	return outcome
-};
-
-if (wet(noAction) === true) {
-	console.log ("The carpet is wet, and a carpet company must extract the water from the carpet.");
-} else {
-	console.log ("The carpet is dry, and no water extraction is needed.");
-}
-
+}; // End Boolean Function
 
 // Number Function
 // While Loop
@@ -78,7 +52,35 @@ var work = function(total) {
 			water+=4;
 	};
 	return water;
-}; // End number function
+}; // End Number Funtion
+
+// Array Function
+// For Loop
+var thingsIBringToWork = function (items, specificThing) {
+	for (i = 0; 
+		 i < 4; 
+		 i++)
+	{
+		console.log(items[i]);
+	}
+		console.log("But I forgot my ");
+		return items[specificThing];
+}; // End Array Function
+
+console.log('\"A resident calls the after hours emergency phone number saying he has a problem with his water heater.\"')
+
+console.log ("When I leave for work, I bring with me my ")
+console.log (thingsIBringToWork(workArray, numberArray) + " at home.");
+
+getWorkRequest(true)
+
+console.log (waterHeater("new water heater."));
+
+if (wet(noAction) === true) {
+	console.log ("The carpet is wet, and a carpet company must extract the water from the carpet.");
+} else {
+	console.log ("The carpet is dry, and no water extraction is needed.");
+}
 
 console.log (work(total) + " gallons of water has been extracted from the carpet.");
 
