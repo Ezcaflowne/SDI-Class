646773f0fe7807927a048654dab417d570ee6bc7
Ezcaflowne <ayotte.willson@gmail.com>
2012-07-15 22:35:39 -0700

    Added bringNewBattery function and started on time workInApartment function. Also debugged some missing puncuation.

--- ayotte_willson_assignment3.js
+++ ayotte_willson_assignment3.js
@@ -12,7 +12,7 @@ var tools = {
     battery: "Lithium Ion",
     typeOfTools: ["Drill", "Sawz-all", "Circular Saw"],
     doesItWork: true,
-    areBladesGood: true
+    areBladesGood: true,
     work: function (apt) {
         console.log("Today we will be working in apt " + apt);
     }
@@ -22,9 +22,26 @@ var goToWork = function () {
         console.log("Sunday through Thursday i wake up at 7:30AM, to make it to work by 8:00AM.");
 };
 
+
+
 var maintenanceCoWorkers = function (coWorkers, dayOff) {
     for (var i = 0, c = coWorkers.length; i < c; i++) {
         var workers = coWorkers[i];
             console.log(workers);
     }   return coWorkers[dayOff]
 };
+
+var bringNewBattery = function (charger, battery) {
+    var getBattery;
+    if (charger === true && batter === true) {
+        getBattery = true;
+    }   else {
+        getBattery = false;
+    }   return getBattery;
+};
+
+
+
+
+
+goToWork();
