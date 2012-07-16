31551ee412ae732dccb7139b4f714f171647b713
Ezcaflowne <ayotte.willson@gmail.com>
2012-07-16 01:18:06 -0700

    Added toolsbladegood function to work with arebladesgood global varible, and console logged it.

--- ayotte_willson_assignment3.js
+++ ayotte_willson_assignment3.js
@@ -11,7 +11,7 @@ var tools = {
     model: "18v",
     battery: "Lithium Ion",
     old: "Nicad",
-    typeOfTools: ["Drill ", "Sawz-all ", "and a Circular Saw"],
+    typeOfTools: ["Drill ", "Sawz-all ", "and a Circular Saw."],
     doesItWork: true,
     areBladesGood: true,
     work: function (apt) {
@@ -27,10 +27,8 @@ var goToWork = function () {
 
 goToWork();
 
-var maintenanceCoWorkers = function (coWorkers, dayOff) 
-{
-    for (var i = 0, c = coWorkers.length; i < c; i++) 
-    {
+var maintenanceCoWorkers = function (coWorkers, dayOff) {
+    for (var i = 0, c = coWorkers.length; i < c; i++) {
         var workers = coWorkers[i];
     }   
     return coWorkers[dayOff];
@@ -48,6 +46,14 @@ var bringNewBattery = function (charger, battery) {
     }   return getBattery;
 };
 
+
+var toolsBladeGood = function(bladeGood) {
+    if (bladeGood === true) { 
+        console.log("Ahh, new blades also make the work go a lot smoother.");
+    } else {
+        console.log("Damn! My blade is shot, first my battery and now this. It's not going to be a very productive day.");
+    }
+};
 
 
 var workInApartment = function (time) {
@@ -81,14 +87,19 @@ console.log(coWorkersArray[1] + ", could you go to the key shop and bring me a n
 if(bringNewBattery(charger, battery) === true) {
         console.log(coWorkersArray[1] + " should have been back by now with my new battery.");
     } else {
-        console.log(coWorkersArray[1] + ' says, your charger wasn\'t plugging in, I don\'t have a battery for you..."');
+        console.log(coWorkersArray[1] + ' says, "your charger wasn\'t plugging in, I don\'t have a battery for you..."');
 };
 
 console.log("I really like my new " + tools.brand +" " + tools.model + tools.battery + " set.");
 console.log("My supervisor " + coWorkersArray[0] + " has the same " + tools.brand + tools.model + " set, but he has the older " 
     + tools.old + " batteries and they dont last nearly as long.");
 
-console.log("Both " + tools.brand + " tool sets contain a " + tools.typeOfTools + " so i always find him with my " + tools.battery
+console.log("Both " + tools.brand + " tool sets contain a " + tools.typeOfTools + " So i always find him with my " + tools.battery
     + " batteries in his tools.");
 console.log("That's probably why my battery is dead...");
+
+toolsBladeGood(tools.areBladesGood);
+
+
+
 
