9b356fbd7ee0bbf3ef34df658a1e93e67f262156
Ezcaflowne <ayotte.willson@gmail.com>
2012-07-23 20:12:15 -0700

    Added first string function to library to verify phone number.

--- sdi-project4/ayotte_willson_assignment4.js
+++ sdi-project4/ayotte_willson_assignment4.js
@@ -1,5 +1,52 @@
 //alert("JavaScript works!");
 
+// Name: Willson Ayotte
+// Date: July 26th 2012
+// Document: Assignment 4 SDI 1207
+// Description: Javascript file for assignment 4
+
 
 /////// Start by making a Library ///////
+
+var myLibrary = function () {
+	
+
+	// Start of Strings //
+
+	//////////////////////////////////
+	// Verify phone number function //
+	//////////////////////////////////
+
+	
+
+	var isItAPhoneNumber = function (phoneNum) {
+ 		if ((phoneNum.charAt(3) === "-") && (phoneNum.charAt(7) === "-")) {
+ 			phoneIs = "This is a phone number.";
+ 		} else {
+ 			phoneIs = "This is not a phone number.";
+ 		};	
+ 		return phoneIs;
+	};
+
+
+
+
+
+
+
+
+
+	return {
+		"isItAPhoneNumber":isItAPhoneNumber
+	};
+
+
+};
+
+/// Make a seperate file that accesses that library ///
+var newLib = new myLibrary();
+
+/// Call that library from this LIB to run the console.log ///
+/// Label what your calling here. ///
+console.log(newLib.isItAPhoneNumber("702-324-4586"));
 
