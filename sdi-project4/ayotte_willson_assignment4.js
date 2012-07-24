b80def404c4738060ff134dec1da699650f2b3fe
Ezcaflowne <ayotte.willson@gmail.com>
2012-07-23 20:46:47 -0700

    Added check email to library, and also added format number to specific decimal.

--- sdi-project4/ayotte_willson_assignment4.js
+++ sdi-project4/ayotte_willson_assignment4.js
@@ -11,13 +11,11 @@
 var myLibrary = function () {
 	
 
-	// Start of Strings //
+// Start of Strings //
 
 	//////////////////////////////////
 	// Verify phone number function //
 	//////////////////////////////////
-
-	
 
 	var isItAPhoneNumber = function (phoneNum) {
  		if ((phoneNum.charAt(3) === "-") && (phoneNum.charAt(7) === "-")) {
@@ -28,16 +26,59 @@ var myLibrary = function () {
  		return phoneIs;
 	};
 
+	//////////////////////////////////////
+	// End verify phone number function //
+	//////////////////////////////////////
 
 
+	///////////////////////////////
+	// Verify of email function ///
+	///////////////////////////////
 
 
+	var emailAddress = function (emailAddy) {
+		if (emailAddy.indexOf(".") === -2) {
+			return "Sorry " + emailAddy + " is not a valid email address."
+		};
+		if (emailAddy.indexOf("@") === -1) {
+			return "Sorry " + emailAddy + " is not a correct email address."
+		} else {
+			return emailAddy + " is a correct email address."
+		}
+	};
+
+	////////////////////////////////
+	// End verify email function ///
+	////////////////////////////////
+
+// End of Strings //
 
 
+// Start of Number //
+
+	///////////////////////////////////////////////////
+	// Start format number to specific decimal place //
+	///////////////////////////////////////////////////
 
 
+	var aNumber = 2.1;
+
+	var getDecimal = function (aNumber) {
+		var format = aNumber.toFixed(2);
+		return format;
+	};
+
+	/////////////////////////////////////////////////
+	// End format number to specific decimal place //
+	/////////////////////////////////////////////////
+
+// End of Number // 
+
+	/// Returned values from functions inside library
 	return {
-		"isItAPhoneNumber":isItAPhoneNumber
+		"isItAPhoneNumber":isItAPhoneNumber, //// Return for Verify phone number function ////
+		"emailAddress":emailAddress, 		 //// Return for Verify of email function ////
+		"getDecimal":getDecimal 			 //// Return for Format number to specific decimal place ////
 	};
 
 
@@ -48,5 +89,15 @@ var newLib = new myLibrary();
 
 /// Call that library from this LIB to run the console.log ///
 /// Label what your calling here. ///
-console.log(newLib.isItAPhoneNumber("702-324-4586"));
+/// Logging of each function to check if they are working. ///
+	
+	/// Loggin Phone Number Verify ///
+	console.log(newLib.isItAPhoneNumber("702-324-4586"));
+	/// Loggin Email Address Verify ///
+	console.log(newLib.emailAddress("ezcaflowne@fullsail.edu"));
+	/// Loggin Format Number to Specific Decimal Places //
+	console.log(newLib.getDecimal(2.1));
+
+
+
 
